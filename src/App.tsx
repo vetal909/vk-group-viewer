059fe13d0cb5d8f5b2cb9c53aff65f097030612b
vetal909 <vitaliekudrya@yandex.ru>
2024-03-10 19:39:48 +0300

    GroupList

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import GroupList from "./сomponents/GroupList/GroupList";
 
 function App() {
--- src/сomponents/GroupItem/GroupItem.tsx
+++ src/сomponents/GroupItem/GroupItem.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import closedIcon from "/svg/close.svg";
+import openIcon from "/svg/open.svg";
 import styles from "./GroupItem.module.css";
 import { Group } from "../../types/types";
 
@@ -7,31 +9,65 @@ interface Props {
 }
 
 const GroupItem: React.FC<Props> = ({ group }) => {
-  const [showFriends, setShowFriends] = useState(false);
-  const toggleFriends = () => {
-    setShowFriends(!showFriends);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
   return (
     <div className={styles.groupItem}>
-      <h3 className={styles.groupName}>{group.name}</h3>
-      <div className={styles.infoContainer}>
-        <p>{group.closed ? "Закрытая" : "Открытая"}</p>
-        <p>Количество участников: {group.members_count}</p>
+      <div
+        className={styles.avatar}
+        style={{ backgroundColor: group.avatar_color }}
+      >
+        {!group.avatar_color && <span className={styles.questionMark}>?</span>}
       </div>
-      {group.avatar_color && (
+
+      <div className={styles.infoContainer}>
+        <h3 className={styles.groupName}>{group.name}</h3>
+        <p className={styles.infotx1}>
+          {group.closed ? (
+            <>
+              <span>Закрытая</span>
+              <img src={closedIcon} alt="closed icon" />
+            </>
+          ) : (
+            <>
+              <span>Открытая</span>
+              <img src={openIcon} alt="open icon" />
+            </>
+          )}
+        </p>
+        <p className={styles.infotxt}>
+          Количество участников:&nbsp;{group.members_count}
+        </p>
+        <div className={styles.friendsBox} onClick={toggleDropdown}>
+          <p className={styles.showFriends}>
+            {group.friends
+              ? group.friends.length > 0
+                ? `Друзей в группе: ${group.friends.length}`
+                : "Нет друзей"
+              : "Нет друзей"}
+          </p>
+          {group.friends && <span className={styles.arrow}>&#9660;</span>}
+        </div>
+      </div>
+
+      {isDropdownOpen && group.friends && (
         <div
-          className={styles.avatar}
-          style={{
-            backgroundColor: group.avatar_color,
-          }}
-        ></div>
-      )}
-      <p onClick={toggleFriends} className={styles.showFriends}>
-        Количество друзей: {group.friends ? group.friends.length : 0}
-      </p>
-      {showFriends && group.friends && (
-        <div className={styles.friends}>
-          <p>Друзья в группе:</p>
+          className={`${styles.friends} ${
+            isDropdownOpen && group.friends ? styles.active : ""
+          }`}
+        >
+          <button className={styles.closeButton} onClick={closeDropdown}>
+            <p className={styles.closeButton}>Х</p>
+          </button>
+          <p className={styles.maininDrop}>Друзья в группе:</p>
           <ul className={styles.friendList}>
             {group.friends.map((friend, index) => (
               <li key={index}>{`${friend.first_name} ${friend.last_name}`}</li>
